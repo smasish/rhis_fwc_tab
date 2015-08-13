3b53f62c03120ee1cce5363be5b77d7205c38990
jamilz <jazaman8@yahoo.com>
2015-08-14 01:08:28 +0600

    Add network permission and add servlet code

---
+++ app/src/main/java/org/sc/alamin/rhis/ClientInfo.java
@@ -0,0 +1,16 @@
+package org.sc.alamin.rhis;
+
+/**
+ * Created by jamil.zaman on 14/08/2015.
+ */
+public class ClientInfo {
+    String mName;
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+}
--- app/src/main/java/org/sc/alamin/rhis/LoginActivity.java
+++ app/src/main/java/org/sc/alamin/rhis/LoginActivity.java
@@ -4,9 +4,10 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends Activity {
 
@@ -16,7 +17,25 @@ public class LoginActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        addListenerOnButton();
+        //addListenerOnButton();
+    }
+
+    public void startLogin(View view) {
+        final EditText passwdText = (EditText)findViewById(R.id.providerPassword);
+        final EditText providerText = (EditText)findViewById(R.id.providerId);
+        //Button loginButton = (Button)findViewById(R.id.buttonLogin);
+        //final TextView loginBanner = (TextView)findViewById(R.id.textViewBanner);
+        ClientInfo client = new ClientInfo();
+        //SendPostRequestAsyncTask
+        SendPostRequestAsyncTask sendPostReqAsyncTask = new SendPostRequestAsyncTask(this);
+        String queryString =   "{" +
+                "uid:" + providerText.getText().toString() + "," +
+                "upass:" + passwdText.getText().toString() + "," +
+                "client:" + "android" +
+                "}";
+        String servlet = "login";
+        String jsonRootkey = "loginInfo";
+        sendPostReqAsyncTask.execute(queryString, servlet, jsonRootkey);
     }
 
     public void addListenerOnButton() {
