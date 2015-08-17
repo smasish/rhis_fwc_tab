89837b235ecf7697843c441cfa68e6aca33ebff2
Mohammod.AlAmin <mohammod.alamin@savethechildren.org>
2015-08-17 09:39:51 +0600

    Embedded of Client's Introduction Fragment into Secondactivity.

---
+++ app/src/main/java/org/sci/rhis/fwc/ClientIntroFragment.java
@@ -0,0 +1,28 @@
+package org.sci.rhis.fwc;
+
+import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class ClientIntroFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_clientinfo,
+                container, false);
+
+        return view;
+    }
+}
+
