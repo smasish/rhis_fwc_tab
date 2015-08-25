303b94f03cde3d869ba8fb5d2442594d25f3bb21
jamilz <jazaman8@yahoo.com>
2015-08-25 15:40:01 +0600

    Add ANCActivity

---
+++ app/src/main/java/org/sci/rhis/fwc/ANCActivityFragment.java
@@ -0,0 +1,22 @@
+package org.sci.rhis.fwc;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ANCActivityFragment extends Fragment {
+
+    public ANCActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_anchistory, container, false);
+    }
+}
