21ecd24927a5c8187d38c4653c8681a008c078aa
jamilz <jazaman8@yahoo.com>
2015-08-30 15:03:47 +0600

    Framework for ANC history

--- app/src/main/java/org/sci/rhis/fwc/ANCActivity.java
+++ app/src/main/java/org/sci/rhis/fwc/ANCActivity.java
@@ -10,6 +10,7 @@ public class ANCActivity extends ClinicalServiceActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_anc);
     }
 
