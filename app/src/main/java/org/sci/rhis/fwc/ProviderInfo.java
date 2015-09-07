fc5a567df877aa42b40ce4974bf94dadc8fd566c
jamilz <jazaman8@yahoo.com>
2015-09-07 08:43:24 +0600

    Make provider Percalable

--- app/src/main/java/org/sci/rhis/fwc/ProviderInfo.java
+++ app/src/main/java/org/sci/rhis/fwc/ProviderInfo.java
@@ -1,9 +1,57 @@
 package org.sci.rhis.fwc;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 /**
  * Created by jamil.zaman on 16/08/2015.
  */
-public class ProviderInfo {
+public class ProviderInfo implements Parcelable {
+
+
+    //Parcelable overrides
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        //"IMPORTANT" ->Do not change the order by which the 'data' is written
+        dest.writeString(mProviderCode);
+        dest.writeString(mProviderName);
+        dest.writeString(mProviderFacility);
+    }
+
+    public static final Parcelable.Creator<ProviderInfo> CREATOR= new Parcelable.Creator<ProviderInfo>() {
+
+        @Override
+        public ProviderInfo createFromParcel(Parcel source) {
+            // TODO Auto-generated method stub
+            return new ProviderInfo(source);  //using parcelable constructor
+        }
+
+        @Override
+        public ProviderInfo[] newArray(int size) {
+            // TODO Auto-generated method stub
+            return new ProviderInfo[size];
+        }
+    };
+
+    //Parcel Constructor
+    public ProviderInfo(Parcel data) {
+        //"IMPORTANT" ->Do not change the order by which the 'data' is accessed
+        mProviderCode       = data.readString();
+        mProviderName       = data.readString();
+        mProviderFacility   = data.readString();
+    }
+
+    public ProviderInfo() {
+    }
+
     public String getProviderCode() {
         return mProviderCode;
     }
