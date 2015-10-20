ae5a3f1f6d993367115408bd5d9d046180dcc69b
smasish <sarker.asish@savethechildren.org>
2015-10-20 09:08:04 +0600

    pnc child history task was almost 90% done but this design changed yesterday..so I have to change it ..now submitting code just for tracking my work what I did yesterday

--- app/src/main/java/org/sci/rhis/fwc/SendPostRequestAsyncTask.java
+++ app/src/main/java/org/sci/rhis/fwc/SendPostRequestAsyncTask.java
@@ -61,6 +61,7 @@ public class SendPostRequestAsyncTask extends AsyncTask<String, Void, String> {
                 // In a POST request, we don't pass the values in the URL.
         //Therefore we use only the web page URL as the parameter of the HttpPost argument
 
+
         HttpPost httpPost = new HttpPost("http://119.148.6.215:8080/RHIS/"+servlet);
        // HttpPost httpPost = new HttpPost("http://10.12.0.32:8080/RHIS/"+servlet);
                 // Because we are not passing values over the URL, we should have a mechanism to pass the values that can be
