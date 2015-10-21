4561db50c97bf30cc508c4ed0dfab9a03678822d
jamilz <jazaman8@yahoo.com>
2015-10-21 10:53:53 +0600

    debug

--- app/src/main/java/org/sci/rhis/fwc/SendPostRequestAsyncTask.java
+++ app/src/main/java/org/sci/rhis/fwc/SendPostRequestAsyncTask.java
@@ -61,7 +61,9 @@ public class SendPostRequestAsyncTask extends AsyncTask<String, Void, String> {
                 // In a POST request, we don't pass the values in the URL.
         //Therefore we use only the web page URL as the parameter of the HttpPost argument
 
-        HttpPost httpPost = new HttpPost("http://119.148.6.215:8080/RHIS/"+servlet);
+        //http://10.12.0.32:8080/RHIS
+        //http://119.148.6.215:8080/RHIS/
+        HttpPost httpPost = new HttpPost("http://10.12.0.32:8080/RHIS/"+servlet);
        // HttpPost httpPost = new HttpPost("http://10.12.0.32:8080/RHIS/"+servlet);
                 // Because we are not passing values over the URL, we should have a mechanism to pass the values that can be
         //uniquely separate by the other end.
