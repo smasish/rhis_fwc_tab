bce22129bf72e8a1c89234753abbe8241331541e
Mohammod.AlAmin <mohammod.alamin@savethechildren.org>
2015-10-25 15:57:45 +0600

    AsyncADVSearchUpdate Activity Created

--- app/src/main/java/org/sci/rhis/fwc/AsyncADVSearchUpdate.java
+++ app/src/main/java/org/sci/rhis/fwc/AsyncADVSearchUpdate.java
@@ -3,5 +3,12 @@ package org.sci.rhis.fwc;
 /**
  * Created by mohammod.alamin on 10/25/2015.
  */
-public class AsyncADVSearchUpdate {
+public class AsyncADVSearchUpdate extends SendPostRequestAsyncTask {
+
+    AsyncADVSearchUpdate(AsyncCallback cb){super(cb);}
+
+    @Override
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+    }
 }
